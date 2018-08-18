d6f5d48b6f85991a199079f1871df0eeafe08379
blueautomatic <42151338+blueautomatic@users.noreply.github.com>
2018-08-18 18:58:52 -0300

    Update E_cuotas.py
    Se agregó el método actualizar_punitorio_de_cta(), el cual revisa cada cuota asociada a ese crédito y aplica el punitorio calculado a cada cuota, y aquellas que no tienen mora quedan sin modificar.
    Además, también actualiza el estado del crédito.

--- E_cuotas.py
+++ E_cuotas.py
@@ -1,4 +1,4 @@
-import sys
+import sys, datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String, Integer, Numeric,func,update,text
 from sqlalchemy import create_engine
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from PyQt5.QtCore import pyqtRemoveInputHook
 from E_configuracion import configuracion
-
+from E_creditos import E_creditos
 
 
 base = declarative_base()
@@ -15,8 +15,9 @@ class E_cuotas(base):
         __tablename__= "cuotas"
         id = Column(Integer, primary_key=True, autoincrement=True)
         nro_credito= Column(Integer)
+
         nro_cuota= Column(Integer)
-        primer_vencimiento=Column(DateTime)
+        primer_Vencimiento=Column(DateTime)
         importe_primer_venc=Column(Numeric)
         estado_cuota=Column(String)
         fecha_cobro=Column(DateTime)
@@ -41,10 +42,11 @@ class E_cuotas(base):
 
         @classmethod
         def guardar(cls, obj_N_cuotas, nro_credito):
+
             new_record = cls(1)
             new_record.nro_credito = nro_credito
-            new_record.nro_cuota = obj_N_cuotas.nro_cuota
-            new_record.primer_vencimiento = str(obj_N_cuotas.primer_Vencimiento)
+            new_record.nro_cuota = int(obj_N_cuotas.nro_cuota)
+            new_record.primer_Vencimiento = str(obj_N_cuotas.primer_Vencimiento)
             new_record.importe_cuota = obj_N_cuotas.importe_primer_vencimiento
             new_record.importe_primer_venc = obj_N_cuotas.importe_primer_vencimiento
             new_record.estado_cuota = obj_N_cuotas.estado_cuota
@@ -55,15 +57,14 @@ class E_cuotas(base):
             new_record.punitorios = obj_N_cuotas.punitorios
             new_record.descripcion = ""
             new_record.write_uid = 0
-            #pyqtRemoveInputHook()
-            #import pdb; pdb.set_trace()
-            calc_interes = (obj_N_cuotas.capital + obj_N_cuotas.interes + obj_N_cuotas.gastos) * 6/ 100
-            obj_redondear = E_cuotas(1)
-            new_record.descuento =obj_redondear.redondear(str(calc_interes))
 
+            calc_interes =  ((obj_N_cuotas.capital + obj_N_cuotas.interes + obj_N_cuotas.gastos) * 6  ) / 100
+            new_record.descuento = 0
+            
             try:
                 new_record.session.add(new_record)
                 new_record.session.commit()
+
                 new_record.session.close()
                 return True
             except :
@@ -73,7 +74,7 @@ class E_cuotas(base):
 
         def get_cuotas_por_nro_credito(self, nro_creditos):
 
-            lista_cuotas = self.session.query(E_cuotas).filter_by(nro_credito = nro_creditos).all()
+            lista_cuotas = self.session.query(E_cuotas).filter_by(nro_credito = nro_creditos).order_by(E_cuotas.nro_cuota.asc()).all()
             try:
                 self.session.close()
                 return lista_cuotas
@@ -89,15 +90,17 @@ class E_cuotas(base):
         def pagar_cuota(self, obj_cuotas):
             #pyqtRemoveInputHook()
             #import pdb; pdb.set_trace()
-            query = update(E_cuotas).where(E_cuotas.id == obj_cuotas.id ).values(estado_cuota=obj_cuotas.estado_cuota, importe_cobrado = obj_cuotas.importe_cobrado, fecha_cobro = obj_cuotas.fecha_cobro,interes = obj_cuotas.interes,gastos = obj_cuotas.gastos, punitorios = obj_cuotas.punitorios, descripcion= obj_cuotas.descripcion, descuento = obj_cuotas.descuento  )
+
+            query = update(E_cuotas).where(E_cuotas.id == obj_cuotas.id).values(estado_cuota=obj_cuotas.estado_cuota, importe_cobrado = obj_cuotas.importe_cobrado, fecha_cobro = obj_cuotas.fecha_cobro,interes = obj_cuotas.interes,gastos = obj_cuotas.gastos, punitorios = obj_cuotas.punitorios, descripcion= obj_cuotas.descripcion, descuento = obj_cuotas.descuento  )
+            self.actualizar_punitorio_de_cta(obj_cuotas.nro_credito)
             self.session.execute(query)
             self.session.commit()
             self.session.close()
 
-        def lista_cuotas_venc_30_dias(self):
+        def lista_cuotas_venc_30_dias(self,slam):
             #pyqtRemoveInputHook()
             #import pdb; pdb.set_trace()
-            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 30 días").all()
+            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 30 dias").all()
             try:
                 self.session.close()
                 return lista_cuotas
@@ -108,7 +111,7 @@ class E_cuotas(base):
         def lista_cuotas_venc_60_dias(self,slam):
             #pyqtRemoveInputHook()
             #import pdb; pdb.set_trace()
-            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 60 días").all()
+            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 60 dias").all()
             try:
                 self.session.close()
                 return lista_cuotas
@@ -119,7 +122,7 @@ class E_cuotas(base):
         def lista_cuotas_venc_90_dias(self,slam):
             #pyqtRemoveInputHook()
             #import pdb; pdb.set_trace()
-            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 90 días").all()
+            lista_cuotas = self.session.query(E_cuotas).filter_by(estado_cuota = "Vencida 90 dias").all()
             try:
                 self.session.close()
                 return lista_cuotas
@@ -139,27 +142,31 @@ class E_cuotas(base):
 
         def generar_punitorios(self,id):
             slam = id
-            sql = text('SELECT miprimerfuncion()')
+            sql = text('SELECT generar_punitorios()')
             result = self.session.execute(sql)
 
             try:
                 self.session.commit()
                 self.session.close()
                 return 1
-            except :
+            except:
                 self.session.close()
                 return False
 
         def buscar_cta_por_nro_cred_y_nro_cta(self, nro_credito, nro_cuota):
             #pyqtRemoveInputHook()
             #import pdb; pdb.set_trace()
-            obj_cuotas = self.session.query(E_cuotas).filter_by(nro_credito = nro_credito,nro_cuota = nro_cuota).first()
-            try:
-                self.session.close()
-                return obj_cuotas
-            except :
-                self.session.close()
-                return False
+            # sql = text("select cta.id as id_cuota, cta.nro_cuota, *, cta.estado_cuota, cta.importe_cobrado, cta.capital, cta.interes, cta.gastos, cta.punitorios, cta.importe_cuota, cta.descripcion, cta.descuento from cuotas as cta inner join credito as cd on cta.nro_credito = cd.nro_credito where cd.nro_credito2 = " + nro_credito + " and cta.nro_cuota = " + nro_cuota)
+            sql = text("select cta.interes as cta_interes, cta.gastos as cta_gastos, * from cuotas as cta inner join credito as cd on cta.nro_credito = cd.nro_credito where cd.nro_credito2 = " + nro_credito + " and cta.nro_cuota = " + nro_cuota)
+            result = self.session.execute(sql)
+            #obj_cuotas = self.session.query(E_cuotas).filter_by(nro_credito = nro_credito,nro_cuota = nro_cuota).first()
+            for x in result:
+                try:
+                    self.session.close()
+                    return x
+                except :
+                    self.session.close()
+                    return False
 
         def actualizar(self, obj_cuotas):
             #pyqtRemoveInputHook()
@@ -175,138 +182,59 @@ class E_cuotas(base):
                 self.session.close()
                 return False
 
-        def actualizar_reparar(self, obj_cuotas):
+        def actualizar_punitorio_de_cta(self, nro_credito):
+            lista_de_cuotas = self.get_cuotas_por_nro_credito(nro_credito)
+            ahora = datetime.datetime.today().date()
+            hoy = datetime.datetime.strptime(str(ahora), '%Y-%m-%d').date()
+            query_cuotas = ""
+            query_credito = ""
+            contador = 0
+            text_estado = ""
 
-                query = update(E_cuotas).where(E_cuotas.id == obj_cuotas.id ).values(estado_cuota=obj_cuotas.estado_cuota, importe_cobrado = obj_cuotas.importe_cobrado, fecha_cobro = obj_cuotas.fecha_cobro,interes = obj_cuotas.interes,gastos = obj_cuotas.gastos, punitorios = obj_cuotas.punitorios, descripcion= obj_cuotas.descripcion, descuento = obj_cuotas.descuento, importe_cuota= obj_cuotas.importe_cuota, importe_primer_venc =obj_cuotas.importe_primer_venc)
-                try:
-                    self.session.execute(query)
-                    self.session.commit()
-                    self.session.close()
-                    return True
-                except:
-                    self.session.rollback()
-                    self.session.close()
-                    return False
+            for item in lista_de_cuotas:
+                fecha_venc = str(item.primer_Vencimiento)[:-9]
+                vencimiento = datetime.datetime.strptime(fecha_venc, '%Y-%m-%d').date()
 
-        def actualizar_refinanciacion(self, obj_cuotas):
-            #pyqtRemoveInputHook()
-            #import pdb; pdb.set_trace()
-            query = update(E_cuotas).where(E_cuotas.id == obj_cuotas.id ).values( importe_cuota = obj_cuotas.importe_cuota, importe_primer_venc= obj_cuotas.importe_primer_venc, primer_vencimiento = obj_cuotas.primer_vencimiento , estado_cuota=obj_cuotas.estado_cuota,capital = obj_cuotas.capital,importe_cobrado = obj_cuotas.importe_cobrado,interes = obj_cuotas.interes,gastos = obj_cuotas.gastos,punitorios = obj_cuotas.punitorios,descripcion= obj_cuotas.descripcion,descuento = obj_cuotas.descuento)
-            try:
-                self.session.execute(query)
-                self.session.commit()
-                self.session.close()
-                return True
-            except:
-                self.session.rollback()
-                self.session.close()
-                return False
+                if item.estado_cuota != 'Pagada':
+                    if vencimiento < hoy: #si la fecha de vencimiento es menor a la fecha de hoy, entonces la cuota está vencida
+                        cantidad_dias_vencidos = vencimiento - hoy
 
+                        print("Los días vencidos de la cuota son " + str(cantidad_dias_vencidos.days) + "\n")
 
+                        if cantidad_dias_vencidos.days < 0:
 
+                            dias_vencidos = (cantidad_dias_vencidos.days * -1) #timedelta arroja hace cuántos días pasó el vencimiento, entonces ese valor está en negativo, porque dice "...days ago"
+                            punitorio = dias_vencidos * (float(item.importe_cuota) * 0.006)
 
-        def get_max_cta(self, nro_credito):
-            sql = text('SELECT max(nro_cuota) as nro_cta from cuotas where nro_credito = ' + str(nro_credito))
-            result = self.session.execute(sql)
-            try:
-                self.session.close()
-                for item in result :
-                    return item.nro_cta
-            except :
-                self.session.close()
-                return False
+                            text_estado = "Cta vencida de " + str(dias_vencidos) + " días"
 
+                            print("La cuota lleva " + str(dias_vencidos) + " días vencida.")
 
-        @classmethod
-        def guardar_refinanciacion(cls, obj_e_cuotas, nro_credito):
+                            query_cuotas = update(E_cuotas).where(E_cuotas.id == item.id).values(punitorios=punitorio, estado_cuota=text_estado, descuento=0)
 
-            new_record = cls(1)
-            new_record.nro_credito = nro_credito
-            new_record.nro_cuota = obj_e_cuotas.nro_cuota
-            new_record.primer_vencimiento = obj_e_cuotas.primer_vencimiento
-            new_record.importe_cuota = obj_e_cuotas.importe_primer_venc
-            new_record.importe_primer_venc = obj_e_cuotas.importe_primer_venc
-            new_record.estado_cuota = obj_e_cuotas.estado_cuota
-            new_record.capital = obj_e_cuotas.capital
-            new_record.interes = obj_e_cuotas.interes
-            new_record.gastos = obj_e_cuotas.gastos
-            new_record.importe_cobrado = 0
-            new_record.punitorios = obj_e_cuotas.punitorios
-            new_record.descripcion = obj_e_cuotas.descripcion
-            new_record.descuento = 0
-            new_record.write_uid = 0
-            try:
-                new_record.session.add(new_record)
-                new_record.session.commit()
-                new_record.session.close()
-                return True
-            except :
-                new_record.session.rollback()
-                new_record.session.close()
-                return False
+                            obj_creditos = E_creditos()
+                            obj_creditos.actualizar_estado_credito(nro_credito, text_estado)
 
-        def redondear(self, obj_capital):
-            #pyqtRemoveInputHook()
-            #import pdb; pdb.set_trace()
-            pos = obj_capital.find('.')
+                            try:
+                                self.session.execute(query_cuotas)
+                                contador = contador + 1
+                                self.session.commit()
+                                self.session.close()
+                                continue
+                            except:
+                                self.session.rollback()
+                                self.session.close()
+                                print(False)
+                                return False
 
-            unidad = obj_capital[(pos-1)]
-            lista = list(obj_capital)
-            result = ""
-            if (int(unidad) <= 3):
-                lista[(pos-1)] = '0'
-            elif (int(unidad) >= 4) :
-                lista[(pos-1)] = '5'
+                print(str(contador))
+                continue
 
-
-            for item in lista:
-                if item != '.' :
-                    result = result + str(item)
-                elif item == '.':
-                    break
-
-            return int(result)
-
-        def reporte_deuda_por_cliente(self):
-            #pyqtRemoveInputHook()
-            #import pdb; pdb.set_trace()
-            sql = text("select round(SUM(c.capital) + SUM(c.interes) + SUM(c.gastos) + SUM(c.punitorios)) "
-                       "as total, pp.apellido, pp.nombre from cuotas c inner join credito ct on "
-                       "c.nro_credito=ct.nro_credito inner join party_party pp on ct.id_party= pp.id_party "
-                       "where c.estado_cuota!='Pagada' group by c.nro_credito,pp.id_party  order by c.nro_credito")
-            result = self.session.execute(sql)
-            try:
-                self.session.close()
-                return result
-            except :
-                self.session.close()
-                return False
-
-        def reporte_cuotas_pagas_por_mes(self):
-
-            sql = text("select (sum(capital) + sum(interes)+ sum(gastos) + sum(punitorios) ) as total, "
-                       "date_trunc('month', primer_vencimiento) AS txn_month, from cuotas "
-                       "where estado_cuota='Pagada' group by txn_month order by txn_month")
-
-            result = self.session.execute(sql)
-            try:
-                self.session.close()
-                return result
-            except :
-                self.session.close()
-                return False
-
-
-
-            def reporte_total_cta_pagar_por_mes(self):
-                sql = text("select (sum(capital) + sum(interes)+ sum(gastos) + sum(punitorios) ) as total ,"
-                           "date_trunc('month', primer_vencimiento) AS txn_month from cuotas "
-                           "where estado_cuota='Pagada' group by txn_month order by txn_month")
-
-                result = self.session.execute(sql)
-                try:
-                    self.session.close()
-                    return result
-                except:
-                    self.session.close()
-                    return False
+            if contador == 0:
+                estado = "Activo"
+                obj_creditos = E_creditos()
+                obj_creditos.actualizar_estado_credito(nro_credito, estado)
+            elif contador > 0 and contador < len(lista_de_cuotas):
+                estado = text_estado
+                obj_creditos = E_creditos()
+                obj_creditos.actualizar_estado_credito(nro_credito, estado)
